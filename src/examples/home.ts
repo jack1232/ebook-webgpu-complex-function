169cc6a1c2b491561e3c74dcb6aadb4dbe9f83ee
jack1232 <jackjack5810@yahoo.com>
2023-09-03 13:14:55 -0400

    initial commit

--- src/examples/home.ts
+++ src/examples/home.ts
@@ -1,7 +1,7 @@
 document.querySelector('.right-div').innerHTML = `
     <div style="text-align:center;">
         <h1>Example Projects for the Book</h1>
-        <h2>WebGPU Marching Cubes</h2>
+        <h2>WebGPU for Complex Function Visualization</h2>
         <h3 style="color:red">For details about this book, please visit 
             <a href="https://drxudotnet.com">https://drxudotnet.com</a></h3>   
     </div>  
